e65068a5860b8c94e619848e7468e2157f1f7533
mdelpech <mdelpech@debian-laptop.myNetwrk>
2018-10-31 21:16:12 +0100

    Add decrement function

--- timer/timer.c
+++ timer/timer.c
@@ -1,10 +1,28 @@
 #include <msp430g2231.h>
 
+void decrement(int *adr_x, int *adr_mask, int seconds) {
+	int i=0;
+	for (i=0;i<seconds;i++) {
+		P1OUT = *adr_x;
+        __delay_cycles(250000);
+        P1OUT = *adr_x & *adr_mask;
+        __delay_cycles(250000);
+        P1OUT = *adr_x;
+        __delay_cycles(250000);
+        P1OUT = *adr_x & *adr_mask;
+        __delay_cycles(250000);
+	    *adr_x = *adr_x >> 1;
+        *adr_mask = *adr_mask >> 1;
+	}
+}
+
 main(void) {
     WDTCTL = WDTPW | WDTHOLD; // Stop watchdog timer
     P1DIR |= 0xFF;
     int x = 0xFF;
+	int *adr_x = &x;
 	int mask = 0x7F;
+	int *adr_mask = &mask;
     /*
     */
     for(;;)
@@ -12,23 +30,13 @@ main(void) {
 		if (~P2IN & 0x01)
 		{
 			x = 0xFF;
-			int mask = 0x7F;
+			mask = 0x7F;
 			P1OUT = x;
 		} else {				
 	    	x = 0xFF;
-			int mask = 0x7F;
+			mask = 0x7F;
 	    	while (x != 0x00) {
-	    		P1OUT = x;
-		    	__delay_cycles(250000);
-				P1OUT = x & mask;
-				__delay_cycles(250000);
-				P1OUT = x;
-		    	__delay_cycles(250000);
-				P1OUT = x & mask;
-				__delay_cycles(250000);
-				P1OUT = x;
-		    	x = x >> 1;
-				mask = mask >> 1;
+				decrement(adr_x, adr_mask, 1);
 				if (~P2IN & 0x01) {
 					break;
 				}
@@ -37,20 +45,3 @@ main(void) {
     }
 }
 
-//travailler sur les pointeurs
-decrement(x, mask, seconds) {
-	int i=0;
-	for (i=0;i<seconds;i++) {
-		P1OUT = x;
-        __delay_cycles(250000);
-        P1OUT = x & mask;
-        __delay_cycles(250000);
-        P1OUT = x;
-        __delay_cycles(250000);
-        P1OUT = x & mask;
-        __delay_cycles(250000);
-        P1OUT = x;
-	    x = x >> 1;
-        mask = mask >> 1;
-	}
-}
