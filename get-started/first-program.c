0aa57e38105193322c422571b025845cbf850886
maxime-delpech <mdelpech@debian-laptop.myNetwrk>
2019-12-02 21:47:52 +0100

    first try

---
+++ get-started/first-program.c
@@ -0,0 +1,17 @@
+#include <msp430.h>
+
+void main(void) {
+	int i;
+
+	WDTCTL = WDTPW | WDTHOLD;
+	
+	P1DIR = 0x01; //LED1 output
+	P1DIR |= 0x10; // LED2 output
+
+	for(;;) {
+		P1OUT ^= 0x11;
+
+		for(i=0;i=10000;i++) {
+		}
+	}
+}
